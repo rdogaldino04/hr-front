e8901215d8d7bccb15d1cecaccfa82c5b659dc22
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-11-13 15:53:58 -0300

    feat: loading component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { HeaderComponent } from '../header/header.component';
 import { MenuModule } from '../shared/components/menu/menu.module';
+import { LoadingModule } from '../shared/components/loading/loading.module';
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RequestInterceptor } from "./user/request.interceptor";
 
 @NgModule({
     declarations: [
@@ -16,7 +19,15 @@ import { MenuModule } from '../shared/components/menu/menu.module';
     imports: [
         CommonModule,
         RouterModule,
-        MenuModule
+        MenuModule,
+        LoadingModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
     ]    
 })
 export class CoreModule { }
