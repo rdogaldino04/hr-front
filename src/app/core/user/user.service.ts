d6a837ead6b6a0cdfed51e57a01e7013e0b9cd91
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-11-13 15:55:33 -0300

    fix: undefed token

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -5,12 +5,12 @@ import { User } from './user';
 import { TokenService } from '../token/token.service';
 import * as jtw_decode from 'jwt-decode';
 
-@Injectable({ providedIn: 'root'})
-export class UserService { 
+@Injectable({ providedIn: 'root' })
+export class UserService {
 
     private userSubject = new BehaviorSubject<User>(null);
     private userName: string;
-    
+
     constructor(private tokenService: TokenService) { }
 
     setToken(token: string) {
@@ -27,14 +27,15 @@ export class UserService {
     }
 
     private decodeAndNotify() {
-        const user = this.decode();        
+        const user = this.decode();
         this.userName = user.name;
         console.log(this.userName)
         this.userSubject.next(user);
-        console.log('this.userSubject.next(user);')
     }
 
     public decode(): User {
+        if (!this.tokenService.hasToken())
+            return null;
         const token = this.tokenService.getToken();
         const payload = jtw_decode.default(token) as jtw_decode.JwtPayload;
         const user = { id: null, name: payload.sub, email: payload.sub };
