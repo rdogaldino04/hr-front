54bffd444ff29cea1c9572b8f7ae94feddd1a3fa
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-11-19 12:26:57 -0300

    refctor: organize imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './core/errors/not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './users/user/user.component';
+import { LoginGuard } from './core/user/login.guard';
 
 const routes: Routes = [
   {
--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -1,30 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
 import { HomeComponent } from './home.component';
-import { SignInComponent } from './signin/signin.component';
-import { SignUpComponent } from './singup/signup.component';
 import { LoginGuard } from '../core/user/login.guard';
 
 const routes: Route[] = [
     {
         path: '',
         component: HomeComponent,
-        canActivate: [LoginGuard],
-            {
-                path: '',
-                component: SignInComponent,
-                data: {
-                    title: 'Sign in'
-                }
-            }, 
-            { 
-                path: 'signup',
-                component: SignUpComponent,
-                data: {
-                    title: 'Sign up'
-                }
-            },
-        ]
+        canActivate: [LoginGuard],       
     }
 
 ]
